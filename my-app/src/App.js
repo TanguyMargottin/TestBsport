27d328cfe2626c0a17ded8ddf1d6eee0c2dbae13
Tanguy Margottin <tanguy.margottin@enseirb-matmeca.fr>
2022-09-21 21:40:21 +0200

    deuxième appel api récupéré la meta-activté

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,47 +4,53 @@ import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
 import moment from 'moment'
 
-export default function UsersData() {
-  const [data, setData] = useState([])
 
-  const apiGet = () => {
-    fetch('https://api.staging.bsport.io/api/v1/offer/?min_date=2022-09-10&max_date=2022-09-11&company=6')
+export default function UsersData() {
+
+  const [offer, setOffer] = useState([])
+  const [activity, setActivity] = useState([])
+  const apiGetOffer = () => {
+    //Get offer from company 6 on the 2022-09-10
+    fetch('https://api.staging.bsport.io/api/v1/offer/?min_date=2022-09-10&max_date=2022-09-10&company=6')
       .then((response) => response.json())
       .then((response) => {
-        console.log(response)
-        setData(response.results)
+        setOffer(response.results)
+      }) 
+    }
+  const apiGetActivity = (id) => {
+    //Get meta-activity from id
+    fetch('https://api.staging.bsport.io/api/v1/meta-activity/'+id+'/')
+      .then((response) => response.json())
+      .then((response) => {
+        setActivity(response)
       })
+  }
+  const [dateState, setDateState] = useState(new Date())
+  const changeDate = (e) => {
+    setDateState(e)
   }
 
   return (
     <div>
       Bsport Get API <br />
-      <button onClick={apiGet}>Fetch API</button>
+      <button onClick={apiGetOffer}>Fetch API offer</button>
       <br />
       <div>
         <ul>
-          {data.map((item) => (
+          {offer.map((item) => (
             <li key={item.id}>
-              {item.id},{item.activity},{item.coach},{item.date_start}
+              {item.date_start},{item.meta_activity},{item.coach},{item.establishment}
             </li>
           ))}
         </ul>
       </div>
-    </div>
-  )
-}
-/**export default function App() {
-  const [dateState, setDateState] = useState(new Date())
-  const changeDate = (e) => {
-    setDateState(e)
-  }
-  return (
-        <>
+      <>
           <Calendar 
           value={dateState}
           onChange={changeDate}
           />
         <p>Current selected date is <b>{moment(dateState).format('MMMM Do YYYY')}</b></p>  
         </>
-      );
-}*/
+    </div>
+  )
+}
